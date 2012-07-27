0118f6149ddfdfdf23816e4cf89ec40f51e03d44
Kamil Kisiel <kamil@kamilkisiel.net>
2012-07-27 10:59:52 -0700

    Made localhost the default graphite server

--- gostatsd/gostatsd.go
+++ gostatsd/gostatsd.go
@@ -2,37 +2,29 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/kisielk/gostatsd/statsd"
 	"log"
-	"os"
 )
 
 var (
-	metricsAddr string
-	consoleAddr string
+	metricsAddr  string
+	consoleAddr  string
 	graphiteAddr string
 )
 
 func init() {
 	const (
-		defaultMetricsAddr = ":8125"
-		defaultConsoleAddr = ":8126"
-		defaultGraphiteAddr = ""
+		defaultMetricsAddr  = ":8125"
+		defaultConsoleAddr  = ":8126"
+		defaultGraphiteAddr = "localhost:2003"
 	)
-	flag.StringVar(&metricsAddr, "l", defaultMetricsAddr, "Address on which to listen for metrics (optional)")
-	flag.StringVar(&consoleAddr, "c", defaultConsoleAddr, "Address on which to listen for console sessions (optional)")
-	flag.StringVar(&graphiteAddr, "g", defaultGraphiteAddr, "Address of the graphite server (required)")
+	flag.StringVar(&metricsAddr, "l", defaultMetricsAddr, "Address on which to listen for metrics")
+	flag.StringVar(&consoleAddr, "c", defaultConsoleAddr, "Address on which to listen for console sessions")
+	flag.StringVar(&graphiteAddr, "g", defaultGraphiteAddr, "Address of the graphite server")
 }
 
 func main() {
 	flag.Parse()
-	if graphiteAddr == "" {
-		fmt.Printf("Error: You must provide the address of the graphite server with the -g flag\n")
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		flag.PrintDefaults()
-		return
-	}
 	err := statsd.ListenAndServe(metricsAddr, consoleAddr, graphiteAddr)
 	if err != nil {
 		log.Fatal(err)
