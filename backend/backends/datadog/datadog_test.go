atlassian/gostatsd

commit bdd144c2e3a32051b3d91574f78d0a5b92bce47a

Rework tags (#52)
Store tags and hostname in aggregated metrics to avoid parsing tags-as-string to get hostname and just tags. Less garbage is generated and less work for cpu.

backend/backends/datadog/datadog.go
@@ -79,8 +79,7 @@ type metric struct {
 type point [2]float64
 
 // AddMetric adds a metric to the series.
-func (ts *timeSeries) addMetric(name, stags, metricType string, value float64, interval time.Duration) {
-	hostname, tags := types.ExtractSourceFromTags(stags)
+func (ts *timeSeries) addMetric(name, metricType, hostname string, value float64, tags types.Tags, interval time.Duration) {
 	if hostname == "" {
 		hostname = ts.Hostname
 	}
@@ -149,34 +148,34 @@ func (d *client) processMetrics(metrics *types.MetricMap, cb func(*timeSeries)) 
 	}
 
 	metrics.Counters.Each(func(key, tagsKey string, counter types.Counter) {
-		ts.addMetric(key, tagsKey, rate, counter.PerSecond, counter.Flush)
-		ts.addMetric(fmt.Sprintf("%s.count", key), tagsKey, gauge, float64(counter.Value), counter.Flush)
+		ts.addMetric(key, rate, counter.Hostname, counter.PerSecond, counter.Tags, counter.Flush)
+		ts.addMetric(fmt.Sprintf("%s.count", key), counter.Hostname, gauge, float64(counter.Value), counter.Tags, counter.Flush)
 		d.maybeFlush(&ts, cb)
 	})
 
 	metrics.Timers.Each(func(key, tagsKey string, timer types.Timer) {
-		ts.addMetric(fmt.Sprintf("%s.lower", key), tagsKey, gauge, timer.Min, timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.upper", key), tagsKey, gauge, timer.Max, timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.count", key), tagsKey, gauge, float64(timer.Count), timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.count_ps", key), tagsKey, rate, timer.PerSecond, timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.mean", key), tagsKey, gauge, timer.Mean, timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.median", key), tagsKey, gauge, timer.Median, timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.std", key), tagsKey, gauge, timer.StdDev, timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.sum", key), tagsKey, gauge, timer.Sum, timer.Flush)
-		ts.addMetric(fmt.Sprintf("%s.sum_squares", key), tagsKey, gauge, timer.SumSquares, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.lower", key), timer.Hostname, gauge, timer.Min, timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.upper", key), timer.Hostname, gauge, timer.Max, timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.count", key), timer.Hostname, gauge, float64(timer.Count), timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.count_ps", key), timer.Hostname, rate, timer.PerSecond, timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.mean", key), timer.Hostname, gauge, timer.Mean, timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.median", key), timer.Hostname, gauge, timer.Median, timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.std", key), timer.Hostname, gauge, timer.StdDev, timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.sum", key), timer.Hostname, gauge, timer.Sum, timer.Tags, timer.Flush)
+		ts.addMetric(fmt.Sprintf("%s.sum_squares", key), timer.Hostname, gauge, timer.SumSquares, timer.Tags, timer.Flush)
 		for _, pct := range timer.Percentiles {
-			ts.addMetric(fmt.Sprintf("%s.%s", key, pct.Str), tagsKey, gauge, pct.Float, timer.Flush)
+			ts.addMetric(fmt.Sprintf("%s.%s", key, pct.Str), timer.Hostname, gauge, pct.Float, timer.Tags, timer.Flush)
 		}
 		d.maybeFlush(&ts, cb)
 	})
 
 	metrics.Gauges.Each(func(key, tagsKey string, g types.Gauge) {
-		ts.addMetric(key, tagsKey, gauge, g.Value, g.Flush)
+		ts.addMetric(key, g.Hostname, gauge, g.Value, g.Tags, g.Flush)
 		d.maybeFlush(&ts, cb)
 	})
 
 	metrics.Sets.Each(func(key, tagsKey string, set types.Set) {
-		ts.addMetric(key, tagsKey, gauge, float64(len(set.Values)), set.Flush)
+		ts.addMetric(key, set.Hostname, gauge, float64(len(set.Values)), set.Tags, set.Flush)
 		d.maybeFlush(&ts, cb)
 	})
 

backend/backends/datadog/datadog_test.go
@@ -104,10 +104,10 @@ func metrics() *types.MetricMap {
 		NumStats: 2,
 		Counters: types.Counters{
 			"stat1": map[string]types.Counter{
-				"tag1": types.NewCounter(time.Now(), 1*time.Second, 5),
+				"tag1": types.NewCounter(time.Now(), 1*time.Second, 5, "", nil),
 			},
 			"stat2": map[string]types.Counter{
-				"tag2": types.NewCounter(time.Now(), 2*time.Second, 50),
+				"tag2": types.NewCounter(time.Now(), 2*time.Second, 50, "", nil),
 			},
 		},
 	}

backend/backends/statsdaemon/statsdaemon_test.go
@@ -15,7 +15,7 @@ var m = types.MetricMap{
 	NumStats: 1,
 	Counters: types.Counters{
 		longName: map[string]types.Counter{
-			"tag1": types.NewCounter(time.Now(), 1*time.Second, 5),
+			"tag1": types.NewCounter(time.Now(), 1*time.Second, 5, "", nil),
 		},
 	},
 }

statsd/aggregator.go
@@ -39,12 +39,13 @@ type aggregator struct {
 	expiryInterval    time.Duration // How often to expire metrics
 	lastFlush         time.Time     // Last time the metrics where aggregated
 	percentThresholds map[float64]percentStruct
-	defaultTags       string // Tags to add to system metrics
+	aggregatorTags    types.Tags // Tags for system metrics
+	aggregatorTagsStr string     // Tags for system metrics (as string)
 	types.MetricMap
 }
 
 // NewAggregator creates a new Aggregator object.
-func NewAggregator(percentThresholds []float64, flushInterval, expiryInterval time.Duration, defaultTags []string) Aggregator {
+func NewAggregator(percentThresholds []float64, flushInterval, expiryInterval time.Duration, aggregatorTags types.Tags) Aggregator {
 	a := aggregator{}
 	a.FlushInterval = flushInterval
 	a.lastFlush = time.Now()
@@ -53,7 +54,8 @@ func NewAggregator(percentThresholds []float64, flushInterval, expiryInterval ti
 	a.Timers = types.Timers{}
 	a.Gauges = types.Gauges{}
 	a.Sets = types.Sets{}
-	a.defaultTags = types.Tags(defaultTags).String()
+	a.aggregatorTags = aggregatorTags
+	a.aggregatorTagsStr = aggregatorTags.SortedString()
 	a.percentThresholds = make(map[float64]percentStruct, len(percentThresholds))
 	for _, pct := range percentThresholds {
 		sPct := strconv.Itoa(int(pct))
@@ -82,7 +84,12 @@ func (a *aggregator) Flush(now func() time.Time) {
 	flushInSeconds := float64(flushInterval.Nanoseconds()) / float64(time.Second.Nanoseconds())
 
 	statName := internalStatName("aggregator_num_stats")
-	a.receiveCounter(statName, a.defaultTags, int64(a.NumStats), startTime)
+	a.receiveCounter(&types.Metric{
+		Name:  statName,
+		Value: float64(a.NumStats),
+		Tags:  a.aggregatorTags,
+		Type:  types.COUNTER,
+	}, a.aggregatorTagsStr, startTime)
 
 	a.Counters.Each(func(key, tagsKey string, counter types.Counter) {
 		counter.PerSecond = float64(counter.Value) / flushInSeconds
@@ -175,7 +182,12 @@ func (a *aggregator) Flush(now func() time.Time) {
 	a.ProcessingTime = flushTime.Sub(startTime)
 
 	statName = internalStatName("processing_time")
-	a.receiveGauge(statName, a.defaultTags, float64(a.ProcessingTime)/float64(time.Millisecond), flushTime)
+	a.receiveGauge(&types.Metric{
+		Name:  statName,
+		Value: float64(a.ProcessingTime) / float64(time.Millisecond),
+		Tags:  a.aggregatorTags,
+		Type:  types.GAUGE,
+	}, a.aggregatorTagsStr, flushTime)
 
 	a.lastFlush = flushTime
 }
@@ -203,8 +215,11 @@ func (a *aggregator) Reset(now time.Time) {
 		if a.isExpired(now, counter.Timestamp) {
 			deleteMetric(key, tagsKey, a.Counters)
 		} else {
-			interval := counter.Interval
-			a.Counters[key][tagsKey] = types.Counter{Interval: interval}
+			a.Counters[key][tagsKey] = types.Counter{
+				Interval: counter.Interval,
+				Hostname: counter.Hostname,
+				Tags:     counter.Tags,
+			}
 		}
 	})
 
@@ -212,8 +227,11 @@ func (a *aggregator) Reset(now time.Time) {
 		if a.isExpired(now, timer.Timestamp) {
 			deleteMetric(key, tagsKey, a.Timers)
 		} else {
-			interval := timer.Interval
-			a.Timers[key][tagsKey] = types.Timer{Interval: interval}
+			a.Timers[key][tagsKey] = types.Timer{
+				Interval: timer.Interval,
+				Hostname: timer.Hostname,
+				Tags:     timer.Tags,
+			}
 		}
 	})
 
@@ -228,84 +246,86 @@ func (a *aggregator) Reset(now time.Time) {
 		if a.isExpired(now, set.Timestamp) {
 			deleteMetric(key, tagsKey, a.Sets)
 		} else {
-			a.Sets[key][tagsKey] = types.Set{Interval: set.Interval, Values: make(map[string]struct{})}
+			a.Sets[key][tagsKey] = types.Set{
+				Values:   make(map[string]struct{}),
+				Interval: set.Interval,
+				Hostname: set.Hostname,
+				Tags:     set.Tags,
+			}
 		}
 	})
 }
 
-func (a *aggregator) receiveCounter(name, tags string, value int64, now time.Time) {
-	v, ok := a.Counters[name]
+func (a *aggregator) receiveCounter(m *types.Metric, tagsKey string, now time.Time) {
+	value := int64(m.Value)
+	v, ok := a.Counters[m.Name]
 	if ok {
-		c, ok := v[tags]
+		c, ok := v[tagsKey]
 		if ok {
 			c.Value += value
 			c.Timestamp = now
 		} else {
-			c = types.NewCounter(now, a.FlushInterval, value)
+			c = types.NewCounter(now, a.FlushInterval, value, m.Hostname, m.Tags)
 		}
-		v[tags] = c
+		v[tagsKey] = c
 	} else {
-		a.Counters[name] = map[string]types.Counter{
-			tags: types.NewCounter(now, a.FlushInterval, value),
+		a.Counters[m.Name] = map[string]types.Counter{
+			tagsKey: types.NewCounter(now, a.FlushInterval, value, m.Hostname, m.Tags),
 		}
 	}
 }
 
-func (a *aggregator) receiveGauge(name, tags string, value float64, now time.Time) {
+func (a *aggregator) receiveGauge(m *types.Metric, tagsKey string, now time.Time) {
 	// TODO: handle +/-
-	v, ok := a.Gauges[name]
+	v, ok := a.Gauges[m.Name]
 	if ok {
-		g, ok := v[tags]
+		g, ok := v[tagsKey]
 		if ok {
-			g.Value = value
+			g.Value = m.Value
 			g.Timestamp = now
 		} else {
-			g = types.NewGauge(now, a.FlushInterval, value)
+			g = types.NewGauge(now, a.FlushInterval, m.Value, m.Hostname, m.Tags)
 		}
-		v[tags] = g
+		v[tagsKey] = g
 	} else {
-		a.Gauges[name] = map[string]types.Gauge{
-			tags: types.NewGauge(now, a.FlushInterval, value),
+		a.Gauges[m.Name] = map[string]types.Gauge{
+			tagsKey: types.NewGauge(now, a.FlushInterval, m.Value, m.Hostname, m.Tags),
 		}
 	}
 }
 
-func (a *aggregator) receiveTimer(name, tags string, value float64, now time.Time) {
-	v, ok := a.Timers[name]
+func (a *aggregator) receiveTimer(m *types.Metric, tagsKey string, now time.Time) {
+	v, ok := a.Timers[m.Name]
 	if ok {
-		t, ok := v[tags]
+		t, ok := v[tagsKey]
 		if ok {
-			t.Values = append(t.Values, value)
+			t.Values = append(t.Values, m.Value)
 			t.Timestamp = now
 		} else {
-			t = types.NewTimer(now, a.FlushInterval, []float64{value})
+			t = types.NewTimer(now, a.FlushInterval, []float64{m.Value}, m.Hostname, m.Tags)
 		}
-		v[tags] = t
+		v[tagsKey] = t
 	} else {
-		a.Timers[name] = map[string]types.Timer{
-			tags: types.NewTimer(now, a.FlushInterval, []float64{value}),
+		a.Timers[m.Name] = map[string]types.Timer{
+			tagsKey: types.NewTimer(now, a.FlushInterval, []float64{m.Value}, m.Hostname, m.Tags),
 		}
 	}
 }
 
-func (a *aggregator) receiveSet(name, tags string, value string, now time.Time) {
-	v, ok := a.Sets[name]
+func (a *aggregator) receiveSet(m *types.Metric, tagsKey string, now time.Time) {
+	v, ok := a.Sets[m.Name]
 	if ok {
-		s, ok := v[tags]
+		s, ok := v[tagsKey]
 		if ok {
-			s.Values[value] = struct{}{}
+			s.Values[m.StringValue] = struct{}{}
 			s.Timestamp = now
 		} else {
-			s = types.NewSet(now, a.FlushInterval, map[string]struct{}{
-				value: {},
-			})
+			s = types.NewSet(now, a.FlushInterval, map[string]struct{}{m.StringValue: {}}, m.Hostname, m.Tags)
 		}
-		v[tags] = s
+		v[tagsKey] = s
 	} else {
-		a.Sets[name] = map[string]types.Set{
-			tags: types.NewSet(now, a.FlushInterval, map[string]struct{}{
-				value: {},
-			}),
+		a.Sets[m.Name] = map[string]types.Set{
+			tagsKey: types.NewSet(now, a.FlushInterval, map[string]struct{}{m.StringValue: {}}, m.Hostname, m.Tags),
 		}
 	}
 }
@@ -313,25 +333,26 @@ func (a *aggregator) receiveSet(name, tags string, value string, now time.Time) 
 // Receive aggregates an incoming metric.
 func (a *aggregator) Receive(m *types.Metric, now time.Time) {
 	a.NumStats++
-	tagsKey := addInstanceIDTag(m.Tags, m.Hostname).String()
+	tagsKey := formatTagsKey(m.Tags, m.Hostname)
 
 	switch m.Type {
 	case types.COUNTER:
-		a.receiveCounter(m.Name, tagsKey, int64(m.Value), now)
+		a.receiveCounter(m, tagsKey, now)
 	case types.GAUGE:
-		a.receiveGauge(m.Name, tagsKey, m.Value, now)
+		a.receiveGauge(m, tagsKey, now)
 	case types.TIMER:
-		a.receiveTimer(m.Name, tagsKey, m.Value, now)
+		a.receiveTimer(m, tagsKey, now)
 	case types.SET:
-		a.receiveSet(m.Name, tagsKey, m.StringValue, now)
+		a.receiveSet(m, tagsKey, now)
 	default:
 		log.Errorf("Unknow metric type %s for %s", m.Type, m.Name)
 	}
 }
 
-func addInstanceIDTag(tags types.Tags, hostname string) types.Tags {
+func formatTagsKey(tags types.Tags, hostname string) string {
+	t := tags.SortedString()
 	if hostname == "" {
-		return tags
+		return t
 	}
-	return append(tags, types.StatsdSourceID+":"+hostname)
+	return t + "," + types.StatsdSourceID + ":" + hostname
 }

commit 15af71a0014c6799e5206963212d7d226b35dec0

Fix wrong hostname argument in Datadog backend (#56)
Was broken in bdd144c2e3a32051b3d91574f78d0a5b92bce47a

backend/backends/datadog/datadog.go
@@ -47,6 +47,7 @@ type client struct {
 	maxRequestElapsedTime time.Duration
 	client                *http.Client
 	metricsPerBatch       uint
+	now                   func() time.Time // Returns current time. Useful for testing.
 }
 
 const sampleConfig = `
@@ -80,7 +81,7 @@ type metric struct {
 type point [2]float64
 
 // AddMetric adds a metric to the series.
-func (ts *timeSeries) addMetric(name, metricType, hostname string, value float64, tags types.Tags) {
+func (ts *timeSeries) addMetric(name, metricType string, value float64, hostname string, tags types.Tags) {
 	if hostname == "" {
 		hostname = ts.hostname
 	}
@@ -145,40 +146,40 @@ func (d *client) processMetrics(metrics *types.MetricMap, cb func(*timeSeries)) 
 	interval := metrics.FlushInterval
 	ts := &timeSeries{
 		Series:           make([]metric, 0, d.metricsPerBatch),
-		timestamp:        float64(time.Now().Unix()),
+		timestamp:        float64(d.now().Unix()),
 		hostname:         d.hostname,
 		flushIntervalSec: interval.Seconds(),
 	}
 
 	metrics.Counters.Each(func(key, tagsKey string, counter types.Counter) {
-		ts.addMetric(key, rate, counter.Hostname, counter.PerSecond, counter.Tags)
-		ts.addMetric(fmt.Sprintf("%s.count", key), counter.Hostname, gauge, float64(counter.Value), counter.Tags)
+		ts.addMetric(key, rate, counter.PerSecond, counter.Hostname, counter.Tags)
+		ts.addMetric(fmt.Sprintf("%s.count", key), gauge, float64(counter.Value), counter.Hostname, counter.Tags)
 		d.maybeFlush(&ts, cb)
 	})
 
 	metrics.Timers.Each(func(key, tagsKey string, timer types.Timer) {
-		ts.addMetric(fmt.Sprintf("%s.lower", key), timer.Hostname, gauge, timer.Min, timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.upper", key), timer.Hostname, gauge, timer.Max, timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.count", key), timer.Hostname, gauge, float64(timer.Count), timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.count_ps", key), timer.Hostname, rate, timer.PerSecond, timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.mean", key), timer.Hostname, gauge, timer.Mean, timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.median", key), timer.Hostname, gauge, timer.Median, timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.std", key), timer.Hostname, gauge, timer.StdDev, timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.sum", key), timer.Hostname, gauge, timer.Sum, timer.Tags)
-		ts.addMetric(fmt.Sprintf("%s.sum_squares", key), timer.Hostname, gauge, timer.SumSquares, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.lower", key), gauge, timer.Min, timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.upper", key), gauge, timer.Max, timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.count", key), gauge, float64(timer.Count), timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.count_ps", key), rate, timer.PerSecond, timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.mean", key), gauge, timer.Mean, timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.median", key), gauge, timer.Median, timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.std", key), gauge, timer.StdDev, timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.sum", key), gauge, timer.Sum, timer.Hostname, timer.Tags)
+		ts.addMetric(fmt.Sprintf("%s.sum_squares", key), gauge, timer.SumSquares, timer.Hostname, timer.Tags)
 		for _, pct := range timer.Percentiles {
-			ts.addMetric(fmt.Sprintf("%s.%s", key, pct.Str), timer.Hostname, gauge, pct.Float, timer.Tags)
+			ts.addMetric(fmt.Sprintf("%s.%s", key, pct.Str), gauge, pct.Float, timer.Hostname, timer.Tags)
 		}
 		d.maybeFlush(&ts, cb)
 	})
 
 	metrics.Gauges.Each(func(key, tagsKey string, g types.Gauge) {
-		ts.addMetric(key, g.Hostname, gauge, g.Value, g.Tags)
+		ts.addMetric(key, gauge, g.Value, g.Hostname, g.Tags)
 		d.maybeFlush(&ts, cb)
 	})
 
 	metrics.Sets.Each(func(key, tagsKey string, set types.Set) {
-		ts.addMetric(key, set.Hostname, gauge, float64(len(set.Values)), set.Tags)
+		ts.addMetric(key, gauge, float64(len(set.Values)), set.Hostname, set.Tags)
 		d.maybeFlush(&ts, cb)
 	})
 
@@ -330,6 +331,7 @@ func NewClient(apiEndpoint, apiKey string, metricsPerBatch uint, clientTimeout, 
 			Timeout: clientTimeout,
 		},
 		metricsPerBatch: metricsPerBatch,
+		now:             time.Now,
 	}, nil
 }
 

backend/backends/datadog/datadog_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/atlassian/gostatsd/types"
 
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 )
 
@@ -43,7 +44,7 @@ func TestRetries(t *testing.T) {
 		t.Fatal(err)
 	}
 	res := make(chan []error, 1)
-	client.SendMetricsAsync(context.Background(), metrics(), func(errs []error) {
+	client.SendMetricsAsync(context.Background(), twoCounters(), func(errs []error) {
 		res <- errs
 	})
 	errs := <-res
@@ -57,7 +58,7 @@ func TestRetries(t *testing.T) {
 	}
 }
 
-func TestSendMetrics(t *testing.T) {
+func TestSendMetricsInMultipleBatches(t *testing.T) {
 	var requestNum uint32
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/v1/series", func(w http.ResponseWriter, r *http.Request) {
@@ -85,7 +86,7 @@ func TestSendMetrics(t *testing.T) {
 		t.Fatal(err)
 	}
 	res := make(chan []error, 1)
-	client.SendMetricsAsync(context.Background(), metrics(), func(errs []error) {
+	client.SendMetricsAsync(context.Background(), twoCounters(), func(errs []error) {
 		res <- errs
 	})
 	errs := <-res
@@ -99,7 +100,54 @@ func TestSendMetrics(t *testing.T) {
 	}
 }
 
-func metrics() *types.MetricMap {
+func TestSendMetrics(t *testing.T) {
+	assert := assert.New(t)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/series", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		data, err := ioutil.ReadAll(r.Body)
+		if !assert.Nil(err) {
+			return
+		}
+		expected := `{"series":[` +
+			`{"host":"h1","interval":1.1,"metric":"c1","points":[[100,1.1]],"tags":["tag1"],"type":"rate"},` +
+			`{"host":"h1","interval":1.1,"metric":"c1.count","points":[[100,5]],"tags":["tag1"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.lower","points":[[100,0]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.upper","points":[[100,1]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.count","points":[[100,1]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.count_ps","points":[[100,1.1]],"tags":["tag2"],"type":"rate"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.mean","points":[[100,0.5]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.median","points":[[100,0.5]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.std","points":[[100,0.1]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.sum","points":[[100,1]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.sum_squares","points":[[100,1]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h2","interval":1.1,"metric":"t1.count_90","points":[[100,0.1]],"tags":["tag2"],"type":"gauge"},` +
+			`{"host":"h3","interval":1.1,"metric":"g1","points":[[100,3]],"tags":["tag3"],"type":"gauge"},` +
+			`{"host":"h4","interval":1.1,"metric":"users","points":[[100,3]],"tags":["tag4"],"type":"gauge"}]}`
+		assert.Equal([]byte(expected), data)
+	})
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+
+	cli, err := NewClient(ts.URL, "apiKey123", 1000, 1*time.Second, 2*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cli.(*client).now = func() time.Time {
+		return time.Unix(100, 0)
+	}
+	res := make(chan []error, 1)
+	cli.SendMetricsAsync(context.Background(), metricsOneOfEach(), func(errs []error) {
+		res <- errs
+	})
+	errs := <-res
+	for _, err := range errs {
+		assert.Nil(err)
+	}
+}
+
+// twoCounters returns two counters.
+func twoCounters() *types.MetricMap {
 	return &types.MetricMap{
 		NumStats: 2,
 		Counters: types.Counters{
@@ -108,6 +156,60 @@ func metrics() *types.MetricMap {
 			},
 			"stat2": map[string]types.Counter{
 				"tag2": types.NewCounter(types.Nanotime(time.Now().UnixNano()), 50, "", nil),
+			},
+		},
+	}
+}
+
+func metricsOneOfEach() *types.MetricMap {
+	return &types.MetricMap{
+		NumStats:       4,
+		ProcessingTime: 10 * time.Millisecond,
+		FlushInterval:  1100 * time.Millisecond,
+		Counters: types.Counters{
+			"c1": map[string]types.Counter{
+				"tag1": {PerSecond: 1.1, Value: 5, Timestamp: types.Nanotime(100), Hostname: "h1", Tags: types.Tags{"tag1"}},
+			},
+		},
+		Timers: types.Timers{
+			"t1": map[string]types.Timer{
+				"tag2": {
+					Count:      1,
+					PerSecond:  1.1,
+					Mean:       0.5,
+					Median:     0.5,
+					Min:        0,
+					Max:        1,
+					StdDev:     0.1,
+					Sum:        1,
+					SumSquares: 1,
+					Values:     []float64{0, 1},
+					Percentiles: types.Percentiles{
+						types.Percentile{Float: 0.1, Str: "count_90"},
+					},
+					Timestamp: types.Nanotime(200),
+					Hostname:  "h2",
+					Tags:      types.Tags{"tag2"},
+				},
+			},
+		},
+		Gauges: types.Gauges{
+			"g1": map[string]types.Gauge{
+				"tag3": {Value: 3, Timestamp: types.Nanotime(300), Hostname: "h3", Tags: types.Tags{"tag3"}},
+			},
+		},
+		Sets: types.Sets{
+			"users": map[string]types.Set{
+				"tag4": {
+					Values: map[string]struct{}{
+						"joe":  {},
+						"bob":  {},
+						"john": {},
+					},
+					Timestamp: types.Nanotime(400),
+					Hostname:  "h4",
+					Tags:      types.Tags{"tag4"},
+				},
 			},
 		},
 	}