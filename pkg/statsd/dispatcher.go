5d957669c531aac1d9073956283b5ade3692c254
hmurn <hmurn@atlassian.com>
2017-08-16 21:59:22 +1000

    Totally shut-down the dispatcher when its done

--- pkg/statsd/dispatcher.go
+++ pkg/statsd/dispatcher.go
@@ -27,18 +27,19 @@ func (f AggregatorFactoryFunc) Create() Aggregator {
 // MetricDispatcher dispatches incoming metrics to corresponding aggregators.
 type MetricDispatcher struct {
 	numWorkers int
-	workers    map[uint16]worker
-	runContext context.Context
+	workers    map[uint16]*worker
+	lock       sync.RWMutex
+	running    bool
 }
 
 // NewMetricDispatcher creates a new NewMetricDispatcher with provided configuration.
 func NewMetricDispatcher(numWorkers int, perWorkerBufferSize int, af AggregatorFactory) *MetricDispatcher {
-	workers := make(map[uint16]worker, numWorkers)
+	workers := make(map[uint16]*worker, numWorkers)
 
 	n := uint16(numWorkers)
 
 	for i := uint16(0); i < n; i++ {
-		workers[i] = worker{
+		workers[i] = &worker{
 			aggr:         af.Create(),
 			metricsQueue: make(chan *gostatsd.Metric, perWorkerBufferSize),
 			processChan:  make(chan *processCommand),
@@ -51,19 +52,28 @@ func NewMetricDispatcher(numWorkers int, perWorkerBufferSize int, af AggregatorF
 	}
 }
 
-// RunAsync runs the MetricDispatcher in a goroutine.
+// RunAsync runs the MetricDispatcher in a goroutine
 func (d *MetricDispatcher) RunAsync(ctx context.Context, done gostatsd.Done) {
-	d.runContext = ctx
-
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	d.running = true
 	go func() {
 		defer done()
+
 		var wg sync.WaitGroup
 		wg.Add(d.numWorkers)
 		for _, worker := range d.workers {
 			w := worker // Make a copy of the loop variable! https://github.com/golang/go/wiki/CommonMistakes
 			go w.work(wg.Done)
 		}
+
 		defer func() {
+			// Once this is indicated as not running, DispatchMetric will no longer
+			// try to send to the channel, so it is safe to close.
+			d.lock.Lock()
+			d.running = false
+			d.lock.Unlock()
+
 			for _, worker := range d.workers {
 				close(worker.metricsQueue) // Close channel to terminate worker
 			}
@@ -89,9 +99,15 @@ func (d *MetricDispatcher) runMetrics(ctx context.Context, statser statser.Stats
 func (d *MetricDispatcher) DispatchMetric(ctx context.Context, m *gostatsd.Metric) error {
 	hash := adler32.Checksum([]byte(m.Name))
 	w := d.workers[uint16(hash%uint32(d.numWorkers))]
+
+	// Protects both the read of d.running, and that the channel remains open
+	d.lock.RLock()
+	defer d.lock.RUnlock()
+	if !d.running {
+		return nil
+	}
+
 	select {
-	case <-d.runContext.Done():
-		return d.runContext.Err()
 	case <-ctx.Done():
 		return ctx.Err()
 	case w.metricsQueue <- m:
@@ -113,9 +129,6 @@ func (d *MetricDispatcher) Process(ctx context.Context, f DispatcherProcessFunc)
 loop:
 	for _, worker := range d.workers {
 		select {
-		case <-d.runContext.Done():
-			wg.Add(cmdSent - d.numWorkers) // Not all commands have been sent, should decrement the WG counter.
-			break loop
 		case <-ctx.Done():
 			wg.Add(cmdSent - d.numWorkers) // Not all commands have been sent, should decrement the WG counter.
 			break loop
