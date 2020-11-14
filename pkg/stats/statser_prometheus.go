110e0c5c19dd61740ace50d3c3c6f10a17651b3a
0AlexZhong0 <0alexzhong0@gmail.com>
2020-11-14 21:56:26 +1300

    Create skeleton methods from the statser interface for prom statser

--- pkg/stats/statser_prometheus.go
+++ pkg/stats/statser_prometheus.go
@@ -2,6 +2,7 @@ package stats
 
 import (
 	"context"
+	"github.com/atlassian/gostatsd"
 	"time"
 )
 
@@ -11,10 +12,36 @@ type PrometheusStatser struct {
 	flushNotifier
 }
 
+// NewPrometheusStatser creates a new Statser which
+// sends internal metrics to prometheus
+func NewPrometheusStatser() Statser {
+	return &PrometheusStatser{}
+}
+
 func (ps *PrometheusStatser) NotifyFlush(ctx context.Context, d time.Duration) {
 	ps.flushNotifier.NotifyFlush(ctx, d)
 }
 
 func (ps *PrometheusStatser) RegisterFlush() (<-chan time.Duration, func()) {
 	return ps.flushNotifier.RegisterFlush()
+}
+
+func (ps *PrometheusStatser) Gauge(name string, value float64, tags gostatsd.Tags) {}
+
+func (ps *PrometheusStatser) Count(name string, amount float64, tags gostatsd.Tags) {}
+
+func (ps *PrometheusStatser) Increment(name string, tags gostatsd.Tags) {}
+
+func (ps *PrometheusStatser) TimingMS(name string, ms float64, tags gostatsd.Tags) {}
+
+func (ps *PrometheusStatser) TimingDuration(name string, d time.Duration, tags gostatsd.Tags) {}
+
+// NewTimer returns a new timer with time set to now
+func (ps *PrometheusStatser) NewTimer(name string, tags gostatsd.Tags) *Timer {
+	return newTimer(ps, name, tags)
+}
+
+// WithTags creates a new Statser with additional tags
+func (ps *PrometheusStatser) WithTags(tags gostatsd.Tags) Statser {
+	return NewTaggedStatser(ps, tags)
 }
