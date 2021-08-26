550beaf2c849f85519106f4bcaa2074c4f5e07be
tiedotguy <tiedotguy@users.noreply.github.com>
2021-08-26 08:18:00 +0000

    Properly remove textContext from pkg/web

--- pkg/web/fixtures_test.go
+++ pkg/web/fixtures_test.go
@@ -2,55 +2,29 @@ package web_test
 
 import (
 	"context"
-	"sync"
-	"time"
 
 	"github.com/atlassian/gostatsd"
 )
 
-type capturingHandler struct {
-	mu   sync.Mutex
-	maps []*gostatsd.MetricMap
-	e    []*gostatsd.Event
+type channeledHandler struct {
+	chMaps chan *gostatsd.MetricMap
 }
 
-func (ch *capturingHandler) EstimatedTags() int {
+func (ch *channeledHandler) EstimatedTags() int {
 	return 0
 }
 
-func (ch *capturingHandler) DispatchMetricMap(ctx context.Context, mm *gostatsd.MetricMap) {
-	ch.mu.Lock()
-	defer ch.mu.Unlock()
-	ch.maps = append(ch.maps, mm)
+func (ch *channeledHandler) DispatchMetricMap(ctx context.Context, mm *gostatsd.MetricMap) {
+	select {
+	case <-ctx.Done():
+	case ch.chMaps <- mm:
+	}
 }
 
-func (ch *capturingHandler) DispatchEvent(ctx context.Context, e *gostatsd.Event) {
-	ch.mu.Lock()
-	defer ch.mu.Unlock()
-	ch.e = append(ch.e, e)
+func (ch *channeledHandler) DispatchEvent(ctx context.Context, e *gostatsd.Event) {
+	panic("events are not supported")
 }
 
-func (ch *capturingHandler) WaitForEvents() {
-}
-
-func (ch *capturingHandler) MetricMaps() []*gostatsd.MetricMap {
-	ch.mu.Lock()
-	defer ch.mu.Unlock()
-	mms := make([]*gostatsd.MetricMap, len(ch.maps))
-	copy(mms, ch.maps)
-	return mms
-}
-
-func testContext() (context.Context, func()) {
-	ctxTest, completeTest := context.WithTimeout(context.Background(), 1500*time.Millisecond)
-	go func() {
-		after := time.NewTimer(1400 * time.Millisecond)
-		select {
-		case <-ctxTest.Done():
-			after.Stop()
-		case <-after.C:
-			panic("test timed out")
-		}
-	}()
-	return ctxTest, completeTest
+func (ch *channeledHandler) WaitForEvents() {
+	panic("events are not supported")
 }
