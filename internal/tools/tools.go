71887c00ef328e68b21eb97961b740f88bdbd852
Sean Marciniak <smarciniak@atlassian.com>
2023-11-28 15:47:15 +1100

    Replacing tooling set up gostatsd that is local

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -1,15 +1,16 @@
+// Package tools is not intended to be consumed by any other package
+// but used to help pin tools.
+
 //go:build tools
 
-package gostatsd
-
-// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+package tools
 
 import (
 	_ "github.com/dvyukov/go-fuzz/go-fuzz"
 	_ "github.com/dvyukov/go-fuzz/go-fuzz-build"
-	_ "github.com/githubnemo/CompileDaemon"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/jstemmer/go-junit-report"
+	_ "github.com/mattn/goveralls"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
