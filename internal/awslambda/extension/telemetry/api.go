dfa592614f0e1547404e9ac77bc5b833c745c4db
Raymond Wang <rwang2@atlassian.com>
2024-01-05 16:14:27 +1100

    Added telemetry server

---
+++ internal/awslambda/extension/telemetry/api.go
@@ -0,0 +1,39 @@
+package telemetry
+
+import "github.com/atlassian/gostatsd/internal/awslambda/extension/api"
+
+const (
+	ApiVersion                              = "2022-07-01"
+	SubscribeEndpoint        api.LambdaPath = "/" + ApiVersion + "/telemetry"
+	ProtocolHTTP             string         = "HTTP"
+	PlatformSubscriptionType string         = "platform"
+	MinBufferingTimeoutMs    int            = 25
+
+	RuntimeDone                EventType = "platform.runtimeDone"
+	LambdaRuntimeAvailableAddr           = "sandbox:8083"
+)
+
+type EventType string
+
+type Event struct {
+	// type is the only field we deserialise as it is used for flushing metrics
+	Type EventType `json:"type"`
+}
+
+type SubscriptionRequest struct {
+	SchemaVersion string                       `json:"schemaVersion"`
+	Types         []string                     `json:"types"`
+	Buffering     *SubscriptionBufferingConfig `json:"buffering,omitempty"`
+	Destination   *SubscriptionDestination     `json:"destination,omitempty"`
+}
+
+type SubscriptionDestination struct {
+	Protocol string `json:"protocol,omitempty"`
+	URI      string `json:"URI"`
+}
+
+type SubscriptionBufferingConfig struct {
+	MaxItems  int         `json:"maxItems,omitempty"`
+	MaxBytes  interface{} `json:"maxBytes,omitempty"`
+	TimeoutMs int         `json:"timeoutMs,omitempty"`
+}
---
+++ internal/awslambda/extension/telemetry/server.go
@@ -0,0 +1,93 @@
+package telemetry
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/sirupsen/logrus"
+)
+
+type RuntimeDoneHook func()
+
+type Server struct {
+	addr string
+	log  logrus.FieldLogger
+	f    RuntimeDoneHook
+}
+
+type ServerOpt func(*Server)
+
+func NewServer(log logrus.FieldLogger, hook RuntimeDoneHook, opts ...ServerOpt) *Server {
+	s := &Server{
+		log:  log,
+		f:    hook,
+		addr: LambdaRuntimeAvailableAddr,
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	return s
+}
+
+func WithCustomAddr(addr string) ServerOpt {
+	return func(s *Server) {
+		s.addr = addr
+	}
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	mx := mux.NewRouter()
+	mx.HandleFunc("/telemetry", s.eventHandler).Methods(http.MethodPost)
+
+	server := &http.Server{Addr: s.addr, Handler: mx}
+
+	go func() {
+		<-ctx.Done()
+		c, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+		err := server.Shutdown(c)
+		if err != nil {
+			s.log.WithError(err).Info("did not shutdown gracefully")
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		s.log.WithError(err).Error("server error")
+		return err
+	}
+
+	s.log.Info("Shutdown telemetry server")
+
+	return nil
+}
+
+func (s *Server) eventHandler(_ http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		s.log.WithError(err).Error("error reading body")
+	}
+
+	var telePayload []Event
+	err = jsoniter.Unmarshal(b, &telePayload)
+	if err != nil {
+		s.log.WithError(err).Error("error unmarshaling telemetry request")
+	}
+
+	for _, p := range telePayload {
+		if p.Type == RuntimeDone {
+			s.f()
+		}
+	}
+}
+
+func (s *Server) Endpoint() string {
+	return fmt.Sprintf("http://%s/telemetry", s.addr)
+}
